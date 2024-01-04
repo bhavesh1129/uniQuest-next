25de1df9d30dab7c89e8b64af629a2c50457afc2
Bhavesh Garg <developerbhaveshgarg@gmail.com>
2024-01-04 20:28:20 +0530

    fix build errors

--- src/utils/getDataFromToken.js
+++ src/utils/getDataFromToken.js
@@ -4,13 +4,9 @@ import jwt from 'jsonwebtoken';
 export const getDataFromToken = async (request) => {
     try {
         const token = await request.cookies.get('token')?.value || '';
-        console.log('Token:', token);
-
         const decodeToken = jwt.verify(token, process.env.TOKEN_SECRET);
-        console.log('Decoded Token:', decodeToken);
-
         return decodeToken.id;
     } catch (error) {
-        throw new Error("Data could not be extracted from token " + error.message);
+        throw new Error("Data could not be extracted from token ", error);
     }
 };
