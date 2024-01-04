78b2f9b36780f99e0d257a0e03a067be46a42808
Bhavesh Garg <developerbhaveshgarg@gmail.com>
2024-01-04 19:45:23 +0530

    fix build errors

--- src/utils/getDataFromToken.js
+++ src/utils/getDataFromToken.js
@@ -1,9 +1,9 @@
 import { NextRequest } from "next/server";
 import jwt from 'jsonwebtoken';
 
-export const getDataFromToken = (request) => {
+export const getDataFromToken = async (request) => {
     try {
-        const token = request.cookies.get('token')?.value || '';
+        const token = await request.cookies.get('token')?.value || '';
         const decodeToken = jwt.verify(token, process.env.TOKEN_SECRET);
         return decodeToken.id;
     } catch (error) {
