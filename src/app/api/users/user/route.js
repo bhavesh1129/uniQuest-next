ad13479f67116a0cea289bfc78c9d375b22f05e4
Bhavesh Garg <developerbhaveshgarg@gmail.com>
2024-01-04 20:16:52 +0530

    fix byuild errors

--- src/app/api/users/user/route.js
+++ src/app/api/users/user/route.js
@@ -1,20 +1,24 @@
 import { NextRequest, NextResponse } from "next/server";
 import { dbConnect } from "@/dbConfig/dbConfig";
 import User from "@/models/userModel";
-import { getDataFromToken } from "@/utils/getDataFromToken";
+import jwt from 'jsonwebtoken';
 
 dbConnect();
 
 export async function GET(request) {
     try {
-        const userId = await getDataFromToken(request);
+        const token = request.cookies.token;
+        if (!token) {
+            return NextResponse.json({ error: "Token not found in cookies", status: 401 });
+        }
+        const decodeToken = jwt.verify(token, process.env.TOKEN_SECRET);
+        const userId = decodeToken.id;
         const user = await User.findOne({ _id: userId }).select("-password");
         if (!user) {
-            return NextResponse.json({ error: "User not found", status: 404 })
+            return NextResponse.json({ error: "User not found", status: 404 });
         }
         return NextResponse.json({ message: "User found", status: 200, data: user });
-
     } catch (error) {
-        return NextResponse.json({ error: "errrrr " + error.message, status: 500 })
+        return NextResponse.json({ error: "Error: " + error.message, status: 500 });
     }
 }
